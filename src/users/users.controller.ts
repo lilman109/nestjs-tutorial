4db249e30668c1c11942bbfa74a74de3e5bd804a
lilman109 <akira.yoshidasjsu@gmail.com>
2024-03-06 10:44:07 +0900

    added temp users controller router

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+    GET    /users
+    GET    /users/:id
+    POST   /users
+    DELETE users/:id
+ */
+
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  // if static route, then add it before a dynamic route
+  // e.g. before /users/:id
+  /* @Get('interns') */
+  /* findAllInterns() { */
+  /*   return []; */
+  /* } */
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
